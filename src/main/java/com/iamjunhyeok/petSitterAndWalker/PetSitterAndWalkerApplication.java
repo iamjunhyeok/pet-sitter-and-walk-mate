1f431d13cdf6e2b1a84968206a6b2686f25648f3
iamjunhyeok <jeonjhyeok@gmail.com>
2023-02-21 02:18:43 +0900

    first commit

---
+++ src/main/java/com/iamjunhyeok/petSitterAndWalker/PetSitterAndWalkerApplication.java
@@ -0,0 +1,13 @@
+package com.iamjunhyeok.petSitterAndWalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSitterAndWalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSitterAndWalkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamjunhyeok/petSitterAndWalker/PetSitterAndWalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamjunhyeok.petSitterAndWalker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSitterAndWalkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
