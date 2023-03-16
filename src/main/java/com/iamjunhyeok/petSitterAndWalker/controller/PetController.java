0b0a89709f8ceb1743302799ae37ca94f0fbdcd9
iamjunhyeok <jeonjhyeok@gmail.com>
2023-03-16 21:20:04 +0900

    내 애완동물 추가 로직 수정

--- src/main/java/com/iamjunhyeok/petSitterAndWalker/controller/PetController.java
+++ src/main/java/com/iamjunhyeok/petSitterAndWalker/controller/PetController.java
@@ -1,13 +1,15 @@
 package com.iamjunhyeok.petSitterAndWalker.controller;
 
-import com.iamjunhyeok.petSitterAndWalker.dto.PetRegisterRequest;
-import com.iamjunhyeok.petSitterAndWalker.dto.PetRegisterResponse;
+import com.iamjunhyeok.petSitterAndWalker.domain.User;
+import com.iamjunhyeok.petSitterAndWalker.dto.MyPetAddRequest;
+import com.iamjunhyeok.petSitterAndWalker.dto.MyPetAddResponse;
 import com.iamjunhyeok.petSitterAndWalker.dto.PetViewResponse;
 import com.iamjunhyeok.petSitterAndWalker.service.PetService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,11 +26,11 @@ public class PetController {
     private final PetService petService;
 
     @PostMapping("/my-pets")
-    public ResponseEntity<PetRegisterResponse> petRegister(@PathVariable Long userId,
-                                                           @RequestPart @Valid PetRegisterRequest request,
-                                                           @RequestPart List<MultipartFile> files) {
+    public ResponseEntity<MyPetAddResponse> addMyPet(@RequestPart @Valid MyPetAddRequest request,
+                                                     @RequestPart List<MultipartFile> files,
+                                                     @AuthenticationPrincipal User user) {
         request.setImages(files);
-        return new ResponseEntity<>(petService.register(userId, request), HttpStatus.CREATED);
+        return new ResponseEntity<>(petService.addMyPet(request, user), HttpStatus.CREATED);
     }
 
     @GetMapping("/my-pets")
