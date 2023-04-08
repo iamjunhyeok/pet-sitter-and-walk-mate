36bade3086ff3b9b2dc8badd38a54c356f989fd8
iamjunhyeok <jeonjhyeok@gmail.com>
2023-04-08 15:17:03 +0900

    펫 시터 Controller 테스트코드 추가

--- src/main/java/com/iamjunhyeok/petSitterAndWalker/petSitter/dto/PetSitterListResponse.java
+++ src/main/java/com/iamjunhyeok/petSitterAndWalker/petSitter/dto/PetSitterListResponse.java
@@ -7,10 +7,23 @@ import lombok.Getter;
 import java.util.List;
 
 @Getter
-@Builder
 public class PetSitterListResponse {
     private String name;
     private String address;
     private float averageRating;
     private List<ImageSimpleDto> images;
+
+    @Builder
+    public PetSitterListResponse(String name, String address, float averageRating, List<ImageSimpleDto> images) {
+        this.name = name;
+        this.address = address;
+        this.averageRating = averageRating;
+        this.images = images;
+    }
+
+    public PetSitterListResponse(String name, String address, float averageRating) {
+        this.name = name;
+        this.address = address;
+        this.averageRating = averageRating;
+    }
 }
